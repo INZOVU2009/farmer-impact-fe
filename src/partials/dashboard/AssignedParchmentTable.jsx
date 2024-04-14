c541ff0cd41c5f7725b2cb7cef7f8a8153bd7c62
Luciefifi <niyomutonilucie@gmail.com>
2024-04-14 10:40:12 +0200

    some changes

--- src/partials/dashboard/AssignedParchmentTable.jsx
+++ src/partials/dashboard/AssignedParchmentTable.jsx
@@ -6,15 +6,11 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import CherryPurchasedCard from "./CherryPurchasedCard";
 import PerchmentAssignment from "../../components/PerchmentAssignment";
-import FarmerPriceCard from "./FarmerPriceCard";
 import { fetchAllAssignedParchments } from "../../redux/actions/parchnment/allAssignedParchment.action";
 import { fetchAllStation } from "../../redux/actions/station/allStations.action";
 import { handleToken } from "../../redux/actions/auth/fetchToken.action";
 import { fetchAllTransactions } from "../../redux/actions/transactions/allTransactions.action";
-
-
 
 // import { jwtDecode } from "jwt-decode";
 
@@ -22,35 +18,34 @@ const AssignedParchmentTable = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { parchment, isloading } = useSelector((state) => state.newParchment);
-  const {allParchments} = useSelector((state)=> state.allAssignedParchments)
-  const [assignedParchments, setAssignedParchments] = useState()
+  const { allParchments } = useSelector((state) => state.allAssignedParchments);
+  const [assignedParchments, setAssignedParchments] = useState();
   const { stations } = useSelector((state) => state.fetchAllStations);
   const [allStation, setAllStation] = useState([]);
   const [allTransactions, setAllTransactions] = useState([]);
-  const token = localStorage.getItem("token")
-  const {decodedToken} = useSelector((state)=> state.fetchToken)
+  const token = localStorage.getItem("token");
+  const { decodedToken } = useSelector((state) => state.fetchToken);
   const { transactions, loading } = useSelector(
     (state) => state.fetchAllTransactions
   );
- 
+
   // const { Certification } = useSelector((state) => state.selectedCertification);
-  
 
-  useEffect(()=>{
-    dispatch(fetchAllAssignedParchments())
-  },[dispatch])
+  useEffect(() => {
+    dispatch(fetchAllAssignedParchments());
+  }, [dispatch]);
 
-  useEffect(()=>{
-    if(allParchments){
-      setAssignedParchments(allParchments.data)
+  useEffect(() => {
+    if (allParchments) {
+      setAssignedParchments(allParchments.data);
     }
-  },[allParchments])
+  }, [allParchments]);
 
-  useEffect(()=>{
-    dispatch(handleToken())
-  },[dispatch])
-  console.log("I am parchm", assignedParchments)
- 
+  useEffect(() => {
+    dispatch(handleToken());
+  }, [dispatch]);
+  console.log("I am parchm", assignedParchments);
+
   const formatDate = (dateString) => {
     const options = {
       year: "numeric",
@@ -84,17 +79,17 @@ const AssignedParchmentTable = () => {
     const station = allStation?.find(
       (station) => station.__kp_Station === _kf_Station
     );
-    return station ? station.StationID: null;
+    return station ? station.StationID : null;
   };
 
   const getTransactionState = (cherry_lot_id) => {
     const transaction = allTransactions?.find(
       (transaction) => transaction.cherry_lot_id === cherry_lot_id
     );
-    return transaction ? transaction.state: null;
+    return transaction ? transaction.state : null;
   };
-   //all transactions
-   useEffect(() => {
+  //all transactions
+  useEffect(() => {
     dispatch(fetchAllTransactions(token));
   }, [dispatch]);
 
@@ -108,7 +103,8 @@ const AssignedParchmentTable = () => {
     (parchment) => getTransactionState(parchment.cherry_lot_id) == "in-drying"
   );
   const inDryStorageParchments = assignedParchments?.filter(
-    (parchment) => getTransactionState(parchment.cherry_lot_id) == "in-dry-storage"
+    (parchment) =>
+      getTransactionState(parchment.cherry_lot_id) == "in-dry-storage"
   );
   const inTransitParchments = assignedParchments?.filter(
     (parchment) => getTransactionState(parchment.cherry_lot_id) == "in-transit"
@@ -117,51 +113,123 @@ const AssignedParchmentTable = () => {
     (parchment) => getTransactionState(parchment.cherry_lot_id) == "delivered"
   );
   const parchmentA = assignedParchments?.filter(
-    (parchment)=> parchment.parch_grade == "Grade A")
-    const parchmentB = assignedParchments?.filter(
-      (parchment)=> parchment.parch_grade == "Grade B")
-      const parchmentC= assignedParchments?.filter(
-        (parchment)=> parchment.parch_grade == "Grade C")
-  const dryingParchmentA = parchmentA?.filter((parchA)=>getTransactionState(parchA.cherry_lot_id) == 'in-drying')
-  const inDryStorageParchmentA = parchmentA?.filter((parchA)=>getTransactionState(parchA.cherry_lot_id) == 'in-dry-storage')
-  const inTransitParchmentA = parchmentA?.filter((parchA) => getTransactionState(parchA.cherry_lot_id) == "in-transit");
-  const inDeliveredParchmentA= parchmentA?.filter((parchA) => getTransactionState(parchA.cherry_lot_id) == "delivered"
+    (parchment) => parchment.parch_grade == "Grade A"
   );
-  const dryingParchmentB = parchmentB?.filter((parchB)=>getTransactionState(parchB.cherry_lot_id) == 'in-drying')
-  const inDryStorageParchmentB= parchmentB?.filter((parchB)=>getTransactionState(parchB.cherry_lot_id) == 'in-dry-storage')
-  const inTransitParchmentB= parchmentB?.filter((parchB) => getTransactionState(parchB.cherry_lot_id) == "in-transit");
-  const inDeliveredParchmentB= parchmentB?.filter((parchB) => getTransactionState(parchB.cherry_lot_id) == "delivered");
-  const dryingParchmentC = parchmentC?.filter((parchC)=>getTransactionState(parchC.cherry_lot_id) == 'in-drying')
-  const inDryStorageParchmentC = parchmentC?.filter((parchC)=>getTransactionState(parchC.cherry_lot_id) == 'in-dry-storage')
-  const inTransitParchmentC= parchmentC?.filter((parchC) => getTransactionState(parchC.cherry_lot_id) == "in-transit");
-  const inDeliveredParchmentC= parchmentC?.filter((parchC) => getTransactionState(parchC.cherry_lot_id) == "delivered");
+  const parchmentB = assignedParchments?.filter(
+    (parchment) => parchment.parch_grade == "Grade B"
+  );
+  const parchmentC = assignedParchments?.filter(
+    (parchment) => parchment.parch_grade == "Grade C"
+  );
+  const dryingParchmentA = parchmentA?.filter(
+    (parchA) => getTransactionState(parchA.cherry_lot_id) == "in-drying"
+  );
+  const inDryStorageParchmentA = parchmentA?.filter(
+    (parchA) => getTransactionState(parchA.cherry_lot_id) == "in-dry-storage"
+  );
+  const inTransitParchmentA = parchmentA?.filter(
+    (parchA) => getTransactionState(parchA.cherry_lot_id) == "in-transit"
+  );
+  const inDeliveredParchmentA = parchmentA?.filter(
+    (parchA) => getTransactionState(parchA.cherry_lot_id) == "delivered"
+  );
+  const dryingParchmentB = parchmentB?.filter(
+    (parchB) => getTransactionState(parchB.cherry_lot_id) == "in-drying"
+  );
+  const inDryStorageParchmentB = parchmentB?.filter(
+    (parchB) => getTransactionState(parchB.cherry_lot_id) == "in-dry-storage"
+  );
+  const inTransitParchmentB = parchmentB?.filter(
+    (parchB) => getTransactionState(parchB.cherry_lot_id) == "in-transit"
+  );
+  const inDeliveredParchmentB = parchmentB?.filter(
+    (parchB) => getTransactionState(parchB.cherry_lot_id) == "delivered"
+  );
+  const dryingParchmentC = parchmentC?.filter(
+    (parchC) => getTransactionState(parchC.cherry_lot_id) == "in-drying"
+  );
+  const inDryStorageParchmentC = parchmentC?.filter(
+    (parchC) => getTransactionState(parchC.cherry_lot_id) == "in-dry-storage"
+  );
+  const inTransitParchmentC = parchmentC?.filter(
+    (parchC) => getTransactionState(parchC.cherry_lot_id) == "in-transit"
+  );
+  const inDeliveredParchmentC = parchmentC?.filter(
+    (parchC) => getTransactionState(parchC.cherry_lot_id) == "delivered"
+  );
 
-  const totalWeightDrying = dryingParchments?.reduce((total, parchment) => total + parseInt(parchment.parch_weight),0);
-  const totalWeightInDryStorage= inDryStorageParchments?.reduce((total, parchment) => total + parseInt(parchment.parch_weight),0);
-  const totalWeightInTransit= inTransitParchments?.reduce((total, parchment) => total + parseInt(parchment.parch_weight),0);
-  const totalWeightInDelivered= inDeliveredParchments?.reduce((total, parchment) => total + parseInt(parchment.parch_weight),0);
+  const totalWeightDrying = dryingParchments?.reduce(
+    (total, parchment) => total + parseInt(parchment.parch_weight),
+    0
+  );
+  const totalWeightInDryStorage = inDryStorageParchments?.reduce(
+    (total, parchment) => total + parseInt(parchment.parch_weight),
+    0
+  );
+  const totalWeightInTransit = inTransitParchments?.reduce(
+    (total, parchment) => total + parseInt(parchment.parch_weight),
+    0
+  );
+  const totalWeightInDelivered = inDeliveredParchments?.reduce(
+    (total, parchment) => total + parseInt(parchment.parch_weight),
+    0
+  );
 
-  const totalDryingA = dryingParchmentA?.reduce( (total,parchA)=> total + parseInt(parchA.parch_weight),0)
-  const totalDryStorageA = inDryStorageParchmentA?.reduce( (total,parchA)=> total + parseInt(parchA.parch_weight),0)
-  const totalTransitA = inTransitParchmentA?.reduce( (total,parchA)=> total + parseInt(parchA.parch_weight),0)
-  const totalDeliveredA = inDeliveredParchmentA?.reduce( (total,parchA)=> total + parseInt(parchA.parch_weight),0)
+  const totalDryingA = dryingParchmentA?.reduce(
+    (total, parchA) => total + parseInt(parchA.parch_weight),
+    0
+  );
+  const totalDryStorageA = inDryStorageParchmentA?.reduce(
+    (total, parchA) => total + parseInt(parchA.parch_weight),
+    0
+  );
+  const totalTransitA = inTransitParchmentA?.reduce(
+    (total, parchA) => total + parseInt(parchA.parch_weight),
+    0
+  );
+  const totalDeliveredA = inDeliveredParchmentA?.reduce(
+    (total, parchA) => total + parseInt(parchA.parch_weight),
+    0
+  );
 
-const totalDryingB = dryingParchmentB?.reduce((total,parchB)=> total + parseInt(parchB.parch_weight),0)
-const totalDryStorageB= inDryStorageParchmentB?.reduce( (total,parchB)=> total + parseInt(parchB.parch_weight),0)
-const totalTransitB = inTransitParchmentB?.reduce( (total,parchB)=> total + parseInt(parchB.parch_weight),0)
-const totalDeliveredB= inDeliveredParchmentB?.reduce( (total,parchB)=> total + parseInt(parchB.parch_weight),0)
+  const totalDryingB = dryingParchmentB?.reduce(
+    (total, parchB) => total + parseInt(parchB.parch_weight),
+    0
+  );
+  const totalDryStorageB = inDryStorageParchmentB?.reduce(
+    (total, parchB) => total + parseInt(parchB.parch_weight),
+    0
+  );
+  const totalTransitB = inTransitParchmentB?.reduce(
+    (total, parchB) => total + parseInt(parchB.parch_weight),
+    0
+  );
+  const totalDeliveredB = inDeliveredParchmentB?.reduce(
+    (total, parchB) => total + parseInt(parchB.parch_weight),
+    0
+  );
 
-const totalDryingC = dryingParchmentC?.reduce((total,parchC)=> total + parseInt(parchC.parch_weight),0)
-const totalDryStorageC = inDryStorageParchmentC?.reduce( (total,parchC)=> total + parseInt(parchC.parch_weight),0)
-const totalTransitC = inTransitParchmentC?.reduce( (total,parchC)=> total + parseInt(parchC.parch_weight),0)
-const totalDeliveredC = inDeliveredParchmentC?.reduce( (total,parchC)=> total + parseInt(parchC.parch_weight),0)
-console.log("I am total weight", totalWeightDrying)
-
-  
+  const totalDryingC = dryingParchmentC?.reduce(
+    (total, parchC) => total + parseInt(parchC.parch_weight),
+    0
+  );
+  const totalDryStorageC = inDryStorageParchmentC?.reduce(
+    (total, parchC) => total + parseInt(parchC.parch_weight),
+    0
+  );
+  const totalTransitC = inTransitParchmentC?.reduce(
+    (total, parchC) => total + parseInt(parchC.parch_weight),
+    0
+  );
+  const totalDeliveredC = inDeliveredParchmentC?.reduce(
+    (total, parchC) => total + parseInt(parchC.parch_weight),
+    0
+  );
+  console.log("I am total weight", totalWeightDrying);
 
   return (
     <div className="flex flex-col col-span-full xl:col-span-12">
-        <div className="p-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
+      <div className="p-4 bg-white dark:bg-slate-800 shadow-lg rounded-sm border border-slate-200 dark:border-slate-700">
         <span className="font-large font-bold  ">Assigned Parchment</span>
         <div className="items-center justify-between block sm:flex md:divide-x md:divide-gray-100 dark:divide-gray-700">
           <div className="flex w-full items-center mb-4  sm:mb-0 ">
@@ -321,82 +389,77 @@ console.log("I am total weight", totalWeightDrying)
             <div className="overflow-hidden shadow">
               <table className="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-600">
                 <thead className="bg-gray-100 dark:bg-gray-700">
-                <tr>
-                   <th scope="col" className="p-4">
-                     PARC-LOT.ID
-                   </th>
-                   <th
-                     scope="col"
-                     className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
-                   >
-                     STATUS
-                   </th>
-                   <th
-                     scope="col"
-                     className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
-                   >
-                     CERTIFICATION
-                   </th>
-                   <th
-                     scope="col"
-                     className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
-                   >
-                     CH-LOT.IDs ASSIGNED
-                   </th>
-                   <th
-                     scope="col"
-                     className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
-                   >
-                     DATE LOT WAS CREATED
-                   </th>
-                   <th
-                     scope="col"
-                     className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
-                   >
-                     GRADE
-                   </th>
-                   <th
-                     scope="col"
-                     className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
-                   >
-                     WEIGHT
-                   </th>
-                 </tr>
-               
+                  <tr>
+                    <th scope="col" className="p-4">
+                      PARC-LOT.ID
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
+                    >
+                      STATUS
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
+                    >
+                      CERTIFICATION
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
+                    >
+                      CH-LOT.IDs ASSIGNED
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
+                    >
+                      DATE LOT WAS CREATED
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
+                    >
+                      GRADE
+                    </th>
+                    <th
+                      scope="col"
+                      className="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400"
+                    >
+                      WEIGHT
+                    </th>
+                  </tr>
                 </thead>
                 <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
-                {assignedParchments?.map((parchment, index) => (
-                  <tr className="hover:bg-gray-100 dark:hover:bg-gray-700">
-                    <td className="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
-                    {parchment.parchment_id}  
-                    </td>
-                    <td className="p-4 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400">
-                    {getTransactionState(parchment.cherry_lot_id)}   
+                  {assignedParchments?.map((parchment, index) => (
+                    <tr className="hover:bg-gray-100 dark:hover:bg-gray-700">
+                      <td className="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                        {parchment.parchment_id}
+                      </td>
+                      <td className="p-4 text-sm font-normal text-gray-500 whitespace-nowrap dark:text-gray-400">
+                        {getTransactionState(parchment.cherry_lot_id)}
+                      </td>
+                      <td className="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                        {parchment.certificate === "1" ? "CP" : "NC"}
+                      </td>
 
-                    </td>
-                    <td className="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
-                    {parchment.certificate === "1" ? "CP":"NC"}
-
-                    </td>
-
-                    <td class="max-w-sm p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400">
-                    {parchment.cherry_lot_id}
-
-                    </td>
-                    <td class="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
-                  {formatDate(parchment.created_at)}
-
-                    </td>
-                    <td className="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
-                  {parchment.parch_grade.charAt(parchment.parch_grade.length - 1)}
-
-                    </td>
-                    <td className="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
-                  {parchment.parch_weight}
-
-                    </td>
-                  </tr>
-                ))}
+                      <td class="max-w-sm p-4 overflow-hidden text-base font-normal text-gray-500 truncate xl:max-w-xs dark:text-gray-400">
+                        {parchment.cherry_lot_id}
+                      </td>
+                      <td class="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                        {formatDate(parchment.created_at)}
+                      </td>
+                      <td className="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                        {parchment.parch_grade.charAt(
+                          parchment.parch_grade.length - 1
+                        )}
+                      </td>
+                      <td className="p-4 text-base font-medium text-gray-500 whitespace-nowrap dark:text-white">
+                        {parchment.parch_weight}
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
