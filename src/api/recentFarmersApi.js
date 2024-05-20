8ca94d47755a03e8cfe3fe6f9e49c2fbd59d00e0
Luciefifi <niyomutonilucie@gmail.com>
2024-05-20 16:15:07 +0200

    format

--- src/api/recentFarmersApi.js
+++ src/api/recentFarmersApi.js
@@ -3,24 +3,22 @@ import axios from "axios";
 // Base URL for the API
 const url = "http://localhost:5000";
 
-
-export const getRecentFarmer = (currentPage,itemsPerPage) => {
-    return new Promise((resolve, reject) => {
-     
-        axios.get(`${url}/user_registration/recentFarmers?page=${currentPage}&&pageSize=${itemsPerPage}`)
-            .then((response) => {
-                // Resolve the promise with the response data if successful
-                console.log(response.data);
-                resolve(response.data);
-                
-            })
-            .catch((error) => {
-                // If an error occurs, check if there's a response data and reject with it
-                if (error.response?.data !== undefined) {
-                    reject(error.response.data);
-                }
-                // Otherwise, reject with the error itself
-                reject(error);
-            });
-    });
+export const getRecentFarmer = (currentPage, itemsPerPage) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(
+        `${url}/user_registration/recentFarmers?page=${currentPage}&&pageSize=${itemsPerPage}`
+      )
+      .then((response) => {
+        resolve(response.data);
+      })
+      .catch((error) => {
+        // If an error occurs, check if there's a response data and reject with it
+        if (error.response?.data !== undefined) {
+          reject(error.response.data);
+        }
+        // Otherwise, reject with the error itself
+        reject(error);
+      });
+  });
 };
